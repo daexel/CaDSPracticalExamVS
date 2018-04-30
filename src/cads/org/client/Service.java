ef568c08428f4f27c5856b99590919914bd726e1
Philipp Beck <BlackDynamite@ws-77-93.haw-1x.haw-hamburg.de>
2018-04-30 16:56:04 +0200

    Und der ganze Rest

--- src/cads/org/client/Service.java
+++ src/cads/org/client/Service.java
@@ -1,22 +1,25 @@
 package cads.org.client;
+
 /**
  * 
  * Service Enumeration
  * 
  * Lists the possible Services for the Roboter
+ * 
  * @author BlackDynamite
  *
  */
 public enum Service {
-	VERTICAL, HORIZONTAL, GRABBER;
-	
-	
+	VERTICAL, HORIZONTAL, GRABBER, ESTOP;
+
 	/**
 	 * parseService
 	 * 
 	 * Parses a String with one of the Service Values into a Service Object.
-	 * @param string to parse
-	 * @return	ServiceObject
+	 * 
+	 * @param string
+	 *            to parse
+	 * @return ServiceObject
 	 */
 	public static Service parseService(String string) {
 		if (string.equals(Service.GRABBER.toString())) {
@@ -25,6 +28,8 @@ public enum Service {
 			return Service.HORIZONTAL;
 		} else if (string.equals(Service.VERTICAL.toString())) {
 			return Service.VERTICAL;
+		} else if (string.equals(Service.ESTOP.toString())) {
+			return Service.ESTOP;
 		} else {
 			throw new IllegalArgumentException(string + " is not part of this enum.");
 		}
