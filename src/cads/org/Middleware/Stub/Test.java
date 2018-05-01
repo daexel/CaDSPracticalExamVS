dade5e98efe486c4bbe466cdf5acfea5d4d6b924
Philipp Beck <BlackDynamite@philipps-air.localdomain>
2018-05-01 18:58:07 +0200

    Test main for testing middleware communication

--- src/cads/org/Middleware/Stub/Test.java
+++ src/cads/org/Middleware/Stub/Test.java
@@ -4,14 +4,15 @@ import cads.org.Middleware.EstopReceiver;
 import cads.org.Middleware.ResponsibiltySide;
 import cads.org.Middleware.RoboterFactory;
 import cads.org.Middleware.ServiceOrderReceiver;
+import cads.org.Middleware.VerticalReceiver;
 import cads.org.client.Order;
 import cads.org.client.Service;
 
 public class Test {
 
 	public static void main(String[] args) {
-		ServiceOrderReceiver estop = new EstopReceiver(1340);
-		RoboterFactory.getService(Service.ESTOP, ResponsibiltySide.CLIENT).move(new Order(0, 0, Service.ESTOP, true));
+		ServiceOrderReceiver estop = new VerticalReceiver(1337);
+		RoboterFactory.getService(Service.VERTICAL, ResponsibiltySide.CLIENT).move(new Order(1, 1, Service.HORIZONTAL, 34));
 		
 	}
 
