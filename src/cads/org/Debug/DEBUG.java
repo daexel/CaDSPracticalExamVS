f8e742a4db0851d459065a2fd11c42311e1fb932
Philipp Beck <BlackDynamite@Philipps-MacBook-Air.local>
2018-05-22 12:24:43 +0200

    DEBUG class with static debugging vars

--- src/cads/org/Debug/DEBUG.java
+++ src/cads/org/Debug/DEBUG.java
@@ -1,7 +1,22 @@
 package cads.org.Debug;
 
 public class DEBUG {
-	public static final boolean STUB_DEBUG = true;
-	public static final boolean SKELETON_DEBUG = true;
-	public static final boolean CLIENT_DEBUG = true;
+
+	// SKELETON DEBUG
+	public static final boolean SKELETON_DEBUG = false;
+	public static final boolean HORIZONTAL_SKELETON_SERVICE = false;
+	public static final boolean VERTICAL_SKELETON_SERVICE = false;
+	public static final boolean GRABBER_SKELETON_SERVICE = false;
+
+	// STUB DEBUG
+	public static final boolean STUB_DEBUG = false;
+	public static final boolean HORIZONTAL_STUB_SERVICE = false;
+	public static final boolean VERTICAL_STUB_SERVICE = false;
+	public static final boolean GRABBER_STUB_SERVICE = false;
+
+	// PARSER DEBUG
+	public static final boolean PARSER_DEBUG = false;
+
+	public static final boolean CLIENT_DEBUG = false;
+
 }
