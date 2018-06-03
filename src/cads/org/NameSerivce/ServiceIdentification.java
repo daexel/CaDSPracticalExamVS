134f4c5eb153b1f8dd96f463d3724d27cce1198a
Philipp Beck <BlackDynamite@philipps-air.localdomain>
2018-06-03 21:31:58 +0200

    Added: Class identifies a service for one roboter with a specific ID

---
+++ src/cads/org/NameSerivce/ServiceIdentification.java
@@ -0,0 +1,43 @@
+package cads.org.NameSerivce;
+
+import cads.org.client.Service;
+
+public class ServiceIdentification {
+	private int roboterID;
+	private Service service;
+
+	public ServiceIdentification(int roboterID, Service service) {
+		this.roboterID = roboterID;
+		this.service = service;
+	}
+
+	/**
+	 * mögliche Fehlerquelle
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		ServiceIdentification o = (ServiceIdentification) obj;
+		if (o.roboterID == this.roboterID & o.service.equals(this.service)) {
+			if (cads.org.Debug.DEBUG.NAME_RESOLUTION) {
+				System.out.println(this.getClass() + ": Same SI objects");
+			}
+			return true;
+		} else {
+			if (cads.org.Debug.DEBUG.NAME_RESOLUTION) {
+				System.out.println(this.getClass() + ": Different SI objects");
+			}
+			return false;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "RoboterID: " + roboterID + " Service: " + service.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		return roboterID + roboterID / (service.toString().toCharArray().length * 3);
+	}
+
+}
