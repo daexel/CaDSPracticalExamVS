a5f24be7704f39b078b28bf46f2e1026b4b1948c
Philipp Beck <BlackDynamite@philipps-air.localdomain>
2018-06-03 21:33:07 +0200

    Changed: Uses the new classes

--- src/cads/org/Middleware/Stub/Test.java
+++ src/cads/org/Middleware/Stub/Test.java
@@ -1,73 +1,103 @@
 package cads.org.Middleware.Stub;
 
-import java.util.Scanner;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
-import cads.org.Middleware.Skeleton.EstopReceiver;
-import cads.org.Middleware.Skeleton.GrabberReceiver;
-import cads.org.Middleware.Skeleton.HorizontalReceiver;
-import cads.org.Middleware.Skeleton.ResponsibiltySide;
-import cads.org.Middleware.Skeleton.RoboterFactory;
-import cads.org.Middleware.Skeleton.ServiceOrderReceiver;
-import cads.org.Middleware.Skeleton.VerticalReceiver;
+import cads.org.Middleware.Skeleton.EstopSkeleton;
+import cads.org.NameSerivce.MiddlewareSide;
 import cads.org.NameSerivce.NameResolution;
 import cads.org.NameSerivce.RegisterModul;
+import cads.org.NameSerivce.RegistryMessageType;
+import cads.org.NameSerivce.RegistryProtocolPaket;
 import cads.org.NameSerivce.RegistryReceiver;
+import cads.org.NameSerivce.ServiceIdentification;
 import cads.org.Server.ServerController;
-import cads.org.client.ClientController;
-import cads.org.client.Order;
 import cads.org.client.Service;
 
 public class Test {
 
 	// What???????????????????
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws Exception {
+		// Test bools
+		boolean serviceTest = false;
+		boolean middlewareNameTest = false;
+		boolean nameResolutionTest = false;
+		boolean nameServiceTest = true;
 
-//		ClientController cc = new ClientController();
-//
-//		ServerController srv = new ServerController();
-//		srv.getRobot().start();
-//
-//		ServiceOrderReceiver estop = new EstopReceiver(1340, srv);
-//		ServiceOrderReceiver vertical = new VerticalReceiver(1337, srv);
-//		ServiceOrderReceiver horizontal = new HorizontalReceiver(1338, srv);
-//		ServiceOrderReceiver grabber = new GrabberReceiver(1339, srv);
-//
-//		// TimeUnit.SECONDS.sleep(10);
-//		// RoboterFactory.getService(Service.GRABBER, ResponsibiltySide.CLIENT)
-//		// .move(new Order(1, 1, Service.GRABBER, 24, true));
-//
-//		Thread.sleep(2000);
-//
-//		 RoboterFactory.getService(Service.VERTICAL, ResponsibiltySide.CLIENT)
-//		 .move(new Order(1, 1, Service.VERTICAL, 49, false));
-//		// TimeUnit.SECONDS.sleep(2);
-//		// RoboterFactory.getService(Service.GRABBER, ResponsibiltySide.CLIENT)
-//		// .move(new Order(1, 1, Service.GRABBER, 24, false));
-//		// TimeUnit.SECONDS.sleep(5);
-//		// // RoboterFactory.getService(Service.ESTOP, ResponsibiltySide.CLIENT)
-//		// // .move(new Order(1, 1, Service.ESTOP, 24, false));
-//		//
-//		// RoboterFactory.getService(Service.VERTICAL, ResponsibiltySide.CLIENT)
-//		// .move(new Order(1, 1, Service.VERTICAL, 20, false));
-//		// RoboterFactory.getService(Service.HORIZONTAL, ResponsibiltySide.CLIENT)
-//		// .move(new Order(1, 1, Service.HORIZONTAL, 10, false));
-//		// TimeUnit.SECONDS.sleep(2);
-//		// RoboterFactory.getService(Service.VERTICAL, ResponsibiltySide.CLIENT)
-//		// .move(new Order(1, 1, Service.VERTICAL, 81, false));
-//		// RoboterFactory.getService(Service.GRABBER, ResponsibiltySide.CLIENT)
-//		// .move(new Order(1, 1, Service.GRABBER, 24, true));
-		
-		NameResolution map = new NameResolution();
-		RegistryReceiver r = new RegistryReceiver(map);
-		RegisterModul rm1 = new RegisterModul();
-	
-		try {
-			rm1.registerService("ReceiverSkeleton", "localhost", 1);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		/**************************************************************************************************/
+
+		// Service Test
+		if (serviceTest) {
+			String skeletonTest;
+			String stubTest;
+			boolean bast = true;
+			for (int i = 0; i < 5; i++) {
+				skeletonTest = Service.values()[i] + "Skeleton";
+				stubTest = Service.values()[i] + "Stub";
+
+				bast = Service.isService(skeletonTest.split("Skeleton")[0]);
+				bast = Service.isService(stubTest.split("Stub")[0]);
+				if (!bast) {
+					System.out.println(skeletonTest + " oder " + stubTest + " scheint nicht zu greifen");
+				}
+			}
+			System.out.println("Alle Services werden erkannt.");
 		}
-		
-	
+
+		/**************************************************************************************************/
+
+		// MiddlewareNameTest
+		if (middlewareNameTest) {
+			String skeletonTest;
+			String stubTest;
+			boolean mw = true;
+			for (int i = 0; i < 5; i++) {
+				skeletonTest = Service.values()[i] + "Skeleton";
+				stubTest = Service.values()[i] + "Stub";
+
+				mw = MiddlewareSide.isSkeleton(skeletonTest);
+				mw = MiddlewareSide.isStub(stubTest);
+				if (!mw) {
+					System.out.println(skeletonTest + " oder " + stubTest + " scheint nicht zu greifen");
+				}
+			}
+			System.out.println("Alle Middlewars werden erkannt.");
+
+		}
+
+		/**************************************************************************************************/
+
+		if (nameResolutionTest) {
+			NameResolution nr = new NameResolution();
+			RegistryProtocolPaket rp = new RegistryProtocolPaket("localhost", "FeedbackStub",
+					RegistryMessageType.REGISTRY_ACCEPTION, 1337, 89, 0);
+			nr.addService(rp);
+			rp = new RegistryProtocolPaket("localhost", "FeedbackStub", RegistryMessageType.REGISTRY_ACCEPTION, 1337,
+					89, 0);
+			nr.addService(rp);
+
+			System.out.println("Port stub can send messages to: "
+					+ nr.getPipline(new ServiceIdentification(rp.getRoboterID(), rp.getService())).getReceiverPort());
+		}
+
+		/**************************************************************************************************/
+
+		// Service Test
+		if (nameServiceTest) {
+			NameResolution nr = new NameResolution();
+			RegistryReceiver r = new RegistryReceiver(nr);
+			int a = RegisterModul.registerStub("FeedbackStub", "localhost", 2);
+			RegisterModul.registerSkeleton("FeedbackSkeleton", "localhost", 2, 1337);
+			System.out.println("huan");
+			DatagramSocket s = new DatagramSocket();
+			byte[] b = new byte[5];
+
+			DatagramPacket p = new DatagramPacket(b, b.length, InetAddress.getLoopbackAddress(), a);
+			s.send(p);
+			s.send(p);s.send(p);
+			s.send(p);s.send(p);s.send(p);s.send(p);s.send(p);
+
+		}
 	}
 }
