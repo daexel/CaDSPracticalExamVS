987ab6ca98f07d4ae27e21263544542b4648f323
Philipp Beck <BlackDynamite@philipps-air.localdomain>
2018-06-03 21:29:22 +0200

    Changed: Changed ServiceStub Classes to Stub

---
+++ src/cads/org/Middleware/Stub/EstopStub.java
@@ -0,0 +1,41 @@
+package cads.org.Middleware.Stub;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+import cads.org.client.Order;
+
+public class EstopStub implements cads.org.Middleware.Skeleton.RoboterService {
+
+	private DatagramSocket serverSocket;
+	private static int port = 1340;
+
+	@Override
+	public void move(Order order) {
+		sendOrder(order);
+	}
+
+	private void sendOrder(Order order) {
+		byte[] b = Order.parseOrder(order);
+		DatagramPacket p = null;
+		try {
+			p = new DatagramPacket(b, b.length, InetAddress.getLocalHost(), port);
+			serverSocket.send(p);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public EstopStub() {
+		try {
+			serverSocket = new DatagramSocket();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
